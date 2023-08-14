3cb948ad04980a3379ea66cb94e6141c3e9a0d22
EGAMER26 <evandrogomes542@gmail.com>
2023-08-14 16:41:38 -0300

    reiniciando projeto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import PokemonCard from '../components/PokemonCard'
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+    </div>
+  )
+}
